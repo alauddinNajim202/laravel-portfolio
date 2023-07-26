f2f4df9dab58be8e2d6e5c35581846de048bf087
alauddin-najim <alauddinnajim202@gmail.com>
2023-07-26 11:44:03 +0600

    PortFolio Laravel

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -36,13 +36,19 @@
             ._image:hover{
                 opacity: 1;;
             }
+
+            .logo img{
+                height: 250px;
+                width: 50px;
+                border-radius: 20px;
+            }
         </style>
     </head>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="..." /></a>
+                <a class="navbar-brand logo rounded-circle "  href="#page-top"><img src="assets/img/logo.png" alt="..." /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
